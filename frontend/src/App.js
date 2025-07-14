a701d0904f8769ce0cbba2d7f449e653b71bf6e5
MAXcecy <iv18_freetime@hotmail.com>
2025-07-14 15:44:35 -0600

    Actualización de texto en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+           <h1>HOLA DESDE NETFILY CON DESPLIEGUE CONTINUO</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
